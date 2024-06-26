ca8e07e543e4b624b123dd53415a7eee99e2aacc
oyenet1 <bowofadeoyerinde@gmail.com>
2024-06-26 05:59:11 +0100

    function done

--- control.rs
+++ control.rs
@@ -1,7 +1,17 @@
 fn main() {
-    if 34 > 45 {
-        println!("Greater");
+    // if 34 > 45 {
+    //     println!("Greater");
+    // } else {
+    //     println!("Less than");
+    // }
+
+    let age = 25;
+    // second if statement
+    if age >= 25 && age <= 40 {
+        println!("You are qualify to work with us");
+    } else if age > 40 {
+        println!("You are too old to work with us");
     } else {
-        println!("Less than");
+        println!("You are too young");
     }
 }
