95122f95d8aa57238ad6346a666c0b6e4652542f
oyenet1 <bowofadeoyerinde@gmail.com>
2024-06-26 06:26:04 +0100

    loop

--- loop.rs
+++ loop.rs
@@ -1,12 +1,30 @@
 fn main() {
-    let mut a = 0;
+    // let mut a = 0;
 
-    loop {
-        if a == 100 {
-            break;
+    // loop {
+    //     if a == 100 {
+    //         break;
+    //     }
+
+    //     println!("{a}");
+    //     a += 1;
+    // }
+
+    fn add_num(num: i32) -> i32 {
+        let mut init = 0;
+        let mut count = 0;
+
+        loop {
+            if count == num {
+                break;
+            }
+
+            count += 1;
+            init += count;
         }
 
-        println!("{a}");
-        a += 1;
+        return init;
     }
+    let res = add_num(4);
+    println!("{res}");
 }
